bf363356db97a3f4b49683c11e9b8d93e88ae5fa
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-04-25 13:33:48 +0100

    Update search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavBar from "./component/NavBar";
+import SearchComponent from "./component/SearchComponent";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <SearchComponent />
     </div>
   );
 }
